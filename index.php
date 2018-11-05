e66cb13a85225c569420a92b61de62a74e3fd721
CitizenBY <44726489+CitizenBY@users.noreply.github.com>
2018-11-05 10:28:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Register</title>
+    
+</head>
+<center><h1> Welcome to our Miage blog </h1>
+<h2>Registration</h2></center>
+<body>
+<center><form action="" method="post"> 
+    <?php 
+    $inputs =array(
+    "Firstname"=>"firstname","Lastname"=>"Lastname",
+    "Username"=>"username","Password"=>"Password","E-mail"=>"email","Phone_Number"=>"Phone Number");
+    foreach ($inputs as $key=>$input)
+    {
+        echo '<input name='.$key.' placeholder='.$input.'></p>';
+    };
+    ?>
+    <p><select name="Gender">
+    <option value="Gender" selected="selected">Gender</option>
+    <option value="Female">Female</option>
+    </select></p>
+    <input type='submit' value='Envoyer'/></center>
+    
+</body>
+</html>
